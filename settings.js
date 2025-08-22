00a843e01677708f7a69005023bf143532ae74c9
silatrix2 <silatrix22@gmail.com>
2025-08-22 11:29:25 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '✨ SILATRIX✨',
+  author: '👑 SILA Hacks',
+  botName: '🤖 SILATRIX',
+  botOwner: 'SILA Hacks', // Your name
+  ownerNumber: '255789661031', // Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: 'public', // can be 'public' or 'private'
+  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by SILA Hacks.',
+  version: '2.0.0',
+};
+
+module.exports = settings;
