b1ead9ebddcf7f4b647a8ae2f96322205fa1310e
silatrix2 <silatrix22@gmail.com>
2025-08-31 05:17:47 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+  packname: 'SILATRIX MD',
+  author: 'SILA TECH',
+  botName: "SILATRIX MD",
+  botOwner: 'SILA TECH',
+  ownerNumber: '255789661031',
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "Moja ya boti bora zaidi za WhatsApp duniani. Imejengwa na SILA TECH.",
+  version: "2.0.0",
+  sessionUrl: "https://beezila.onrender.com/"
+};
+
+module.exports = settings;
